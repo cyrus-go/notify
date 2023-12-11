7bde16c749ec5e5ff6b4a88f95e7fdfcb840af3f
cyrus@cephalon.ai <cyrus@cephalon.ai>
2023-12-11 18:44:10 +0800

    feat: add email register cephalon core

--- mail/business.go
+++ mail/business.go
@@ -3,11 +3,14 @@ package mail
 import "fmt"
 
 const (
-	cephalonRegisterContext = "欢迎注册 Cephalon Core，本次注册的验证码为 %d，5分钟内有效"
+	cephalonRegisterContext     = "【端脑科技】欢迎注册 Cephalon Cloud，本次注册的验证码为 %d，5分钟内有效"
+	cephalonCoreRegisterContext = "【端脑科技】欢迎注册 Cephalon Core，本次注册的验证码为 %d，5分钟内有效"
 
-	sendNameCephalonRegister = "端脑云"
+	sendNameCephalonRegister     = "端脑云"
+	sendNameCephalonCoreRegister = "端脑小程序"
 
-	titleCephalonRegister = "端脑云注册验证"
+	titleCephalonRegister     = "端脑云注册验证"
+	titleCephalonCoreRegister = "端脑小程序注册验证"
 )
 
 type VerifyMail struct {
@@ -28,7 +31,12 @@ func NewVerifyMail(username, password, sendMail, receiveMail string, code int) *
 	}
 }
 
-// SendMailCephalonRegister 端脑云注册
+// SendMailCephalonRegister Cephalon 注册
 func (v *VerifyMail) SendMailCephalonRegister() error {
 	return SendMailAli(v.Username, v.Password, v.SendMail, v.ReceiveMail, sendNameCephalonRegister, titleCephalonRegister, fmt.Sprintf(cephalonRegisterContext, v.Code))
+}
+
+// SendMailCephalonCoreRegister Cephalon Core 注册
+func (v *VerifyMail) SendMailCephalonCoreRegister() error {
+	return SendMailAli(v.Username, v.Password, v.SendMail, v.ReceiveMail, sendNameCephalonCoreRegister, titleCephalonCoreRegister, fmt.Sprintf(cephalonCoreRegisterContext, v.Code))
 }
