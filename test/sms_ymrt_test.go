bba2e6c5ec380d5f9b6b32dbcee5d46513d94d0c
cyrus <cyrus@cephalon.ai>
2024-11-29 10:44:21 +0800

    fix: add test custom

--- test/sms_ymrt_test.go
+++ test/sms_ymrt_test.go
@@ -57,7 +57,21 @@ func TestSmsYmrtNotify(t *testing.T) {
 	content := "【端脑科技】温馨提示：您正在使用的 AI 应用即将到期，系统显示您的余额不足以进行自动续费，如需继续使用请及时充值噢！到期后应用会自动停用并清除现有设置，为了方便您的使用，请确保在服务到期前进行充值或备份数据。\n    端脑云感谢您的使用，祝您生活愉快！"
 	smsCli, err := sms.NewNotifySmsYmrt(appID, secretKey, mobile)
 	if err != nil {
-		t.Logf("init verify sms failed, err: %v", err)
+		t.Logf("init notify sms failed, err: %v", err)
+		return
+	}
+	// 发送模板内容
+	if err = smsCli.SendSmsCustom(content); err != nil {
+		t.Fatalf("send notify cephalon sms failed, err: %v", err)
+	}
+}
+
+func TestSmsYmrtCustom(t *testing.T) {
+	mobile := "13187098660"
+	content := "【端脑科技】您有一个应用已运行 6 小时，为了避免脑力值浪费，如闲置请及时关闭。应用编号：1860874412931571712"
+	smsCli, err := sms.NewNotifySmsYmrt(appID, secretKey, mobile)
+	if err != nil {
+		t.Logf("init notify sms failed, err: %v", err)
 		return
 	}
 	// 发送模板内容
